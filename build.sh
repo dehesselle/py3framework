bb0da2704d53bb29404294fb24ec15fbf793e166
René de Hesselle <dehesselle@web.de>
2019-06-02 23:40:58 +0200

    Update prototype

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ URL_PYTHON=https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz
 URL_OPENSSL=https://www.openssl.org/source/openssl-1.1.0g.tar.gz
 URL_READLINE=ftp://ftp.cwru.edu/pub/bash/readline-7.0.tar.gz
 URL_GETTEXT=https://ftp.gnu.org/pub/gnu/gettext/gettext-0.19.8.tar.gz
+URL_XZUTILS=https://tukaani.org/xz/xz-5.2.4.tar.xz
 
 
 ### FUNCTIONS ##################################################################
@@ -91,12 +92,12 @@ function reset_dylib_name
   local file=$1   # dylib filename
 
   local name=$(otool -D $file | tail -n 1)
-  otool -id $(basename $name) $file   # remote path from library id
+  install_name_tool -id $(basename $name) $file   # remote path from library id
 }
 
 ### MAIN #######################################################################
 
-set -e
+#set -e
 
 export CFLAGS=-I$INC_DIR
 export CXXFLAGS=-I$INC_DIR
@@ -112,6 +113,9 @@ configure_make_makeinstall
 get_source $URL_GETTEXT
 configure_make_makeinstall --without-emacs --disable-java --disable-native-java --disable-libasprintf --disable-csharp
 
+get_source $URL_XZUTILS
+configure_make_makeinstall
+
 unset MAKEFLAGS
 
 get_source $URL_PYTHON
@@ -119,16 +123,56 @@ configure_make_makeinstall --enable-framework=$OPT_DIR/Frameworks
 
 
 # add 3rd party libraries to Framework
+FRA_DIR=$OPT_DIR/Frameworks/Python.framework
+FRA_EXT_LIB_DIR=$FRA_DIR/Versions/3.6/Libraries
+FRA_RES_DIR=$FRA_DIR/Versions/3.6/Resources
+FRA_BIN_DIR=$FRA_DIR/Versions/3.6/bin
+FRA_LIB_DIR=$FRA_DIR/Versions/3.6/lib
+FRA_PY3_LIB=$FRA_DIR/Versions/3.6/Python
+mkdir -p $FRA_EXT_LIB_DIR
+ln -s $FRA_EXT_LIB_DIR $FRA_DIR
+
+
+
+cp $LIB_DIR/libintl.9.dylib $FRA_EXT_LIB_DIR
+reset_dylib_name $FRA_EXT_LIB_DIR/libintl.9.dylib
+chmod 755 $FRA_PY3_LIB
+install_name_tool -change $LIB_DIR/libintl.9.dylib @loader_path/Libraries/libintl.9.dylib $FRA_PY3_LIB
+chmod 555 $FRA_PY3_LIB
+
+cp $LIB_DIR/libreadline.7.dylib $FRA_EXT_LIB_DIR
+chmod 644 $FRA_EXT_LIB_DIR/libreadline.7.dylib 
+reset_dylib_name $FRA_EXT_LIB_DIR/libreadline.7.dylib
+install_name_tool -change $LIB_DIR/libreadline.7.dylib @loader_path/../../../Libraries/libreadline.7.dylib $FRA_LIB_DIR/python3.6/lib-dynload/readline.cpython-36m-darwin.so
+
+cp $LIB_DIR/libssl.1.1.dylib $FRA_EXT_LIB_DIR
+chmod 644 $FRA_EXT_LIB_DIR/libssl.1.1.dylib
+reset_dylib_name $FRA_EXT_LIB_DIR/libssl.1.1.dylib
+install_name_tool -change $LIB_DIR/libcrypto.1.1.dylib @loader_path/libcrypto.1.1.dylib $FRA_EXT_LIB_DIR/libssl.1.1.dylib
+install_name_tool -change $LIB_DIR/libssl.1.1.dylib @loader_path/../../../Libraries/libssl.1.1.dylib $FRA_LIB_DIR/python3.6/lib-dynload/_hashlib.cpython-36m-darwin.so
+install_name_tool -change $LIB_DIR/libssl.1.1.dylib @loader_path/../../../Libraries/libssl.1.1.dylib $FRA_LIB_DIR/python3.6/lib-dynload/_ssl.cpython-36m-darwin.so
+
+cp $LIB_DIR/libcrypto.1.1.dylib $FRA_EXT_LIB_DIR
+chmod 644 $FRA_EXT_LIB_DIR/libcrypto.1.1.dylib
+reset_dylib_name $FRA_EXT_LIB_DIR/libcrypto.1.1.dylib
+install_name_tool -change $LIB_DIR/libcrypto.1.1.dylib @loader_path/../../../Libraries/libcrypto.1.1.dylib $FRA_LIB_DIR/python3.6/lib-dynload/_hashlib.cpython-36m-darwin.so
+install_name_tool -change $LIB_DIR/libcrypto.1.1.dylib @loader_path/../../../Libraries/libcrypto.1.1.dylib $FRA_LIB_DIR/python3.6/lib-dynload/_ssl.cpython-36m-darwin.so
+
+cp $LIB_DIR/liblzma.5.dylib $FRA_EXT_LIB_DIR
+chmod 644 $FRA_EXT_LIB_DIR/liblzma.5.dylib
+reset_dylib_name $FRA_EXT_LIB_DIR/liblzma.5.dylib
+install_name_tool -change $LIB_DIR/liblzma.5.dylib @loader_path/../../../Libraries/liblzma.5.dylib $FRA_LIB_DIR/python3.6/lib-dynload/_lzma.cpython-36m-darwin.so
+
+
+
 
 
 
 # make library link paths relative
-install_name_tool -change $OPT_DIR/Frameworks/Python.framework/Versions/3.6/Python @executable_path/../../../Versions/3.6/Python $OPT_DIR/Frameworks/Python.framework/Versions/3.6/bin/python3.6
-install_name_tool -change $OPT_DIR/Frameworks/Python.framework/Versions/3.6/Python @executable_path/../../../Versions/3.6/Python $OPT_DIR/Frameworks/Python.framework/Versions/3.6/bin/python3.6m
-install_name_tool -change $OPT_DIR/Frameworks/Python.framework/Versions/3.6/Python @executable_path/../../../../Python $OPT_DIR/Frameworks/Python.framework/Versions/3.6/Resources/Python.app/Contents/MacOS/Python
-
-
-
+install_name_tool -change $FRA_PY3_LIB @executable_path/../Python $FRA_BIN_DIR/python3.6
+install_name_tool -change $FRA_PY3_LIB @executable_path/../Python $FRA_BIN_DIR/python3.6m
+install_name_tool -change $FRA_PY3_LIB @executable_path/../../../../Python $FRA_RES_DIR/Python.app/Contents/MacOS/Python
+install_name_tool -change $LIB_DIR/libintl.9.dylib @executable_path/../../../../Libraries/libintl.9.dylib $FRA_RES_DIR/Python.app/Contents/MacOS/Python
 
 # replace hard-coded interpreter path in shebang with an environment lookup
 PYTHON_BIN_DIR=$OPT_DIR/Frameworks/Python.framework/Versions/3.6/bin
