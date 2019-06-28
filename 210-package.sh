6bed3b47d1ea6e9bdb4634aec0e1558b05bd06fe
René de Hesselle <dehesselle@web.de>
2019-06-28 19:23:33 +0200

    Remove executable flag from zlib

--- 210-package.sh
+++ 210-package.sh
@@ -53,6 +53,7 @@ install_name_tool -change $LIB_DIR/liblzma.5.dylib @loader_path/../../../Librari
 ### add zlib ###################################################################
 
 cp $LIB_DIR/libz.1.2.11.dylib $FRA_EXT_LIB_DIR
+chmod 644 $FRA_EXT_LIB_DIR/libz.1.2.11.dylib
 reset_dylib_name $FRA_EXT_LIB_DIR/libz.1.2.11.dylib
 install_name_tool -change $LIB_DIR/libz.1.dylib @loader_path/../../../Libraries/libz.1.2.11.dylib $FRA_LIB_DIR/python3.6/lib-dynload/zlib.cpython-36m-darwin.so
 install_name_tool -change $LIB_DIR/libz.1.dylib @loader_path/../../../Libraries/libz.1.2.11.dylib $FRA_LIB_DIR/python3.6/lib-dynload/binascii.cpython-36m-darwin.so
